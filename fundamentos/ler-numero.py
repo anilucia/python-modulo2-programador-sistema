119173788be7ffca067e50402af7539ffedb4497
anilucia <amilucia07@gmail.com>
2026-03-13 20:49:36 -0300

    Ler a média entre dois números

---
+++ fundamentos/ler-numero.py
@@ -0,0 +1,5 @@
+#7.Leia dois números decimais (float) e imprima a média. 
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+media = (numero1 + numero2) / 2
+print(f"A média de {numero1} e {numero2} é {media}.")
