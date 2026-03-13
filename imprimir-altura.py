d9a6f22b0f8ab603aae8d43f92569d3f795c4290
anilucia <amilucia07@gmail.com>
2026-03-13 20:42:11 -0300

    Imprimindo altura

---
+++ imprimir-altura.py
@@ -0,0 +1,3 @@
+#6.Leia a altura (em metros) como float e imprima: "Sua altura é {altura}m" formato da entrada float 1.50
+altura = float(input("Digite sua altura em metros: "))
+print(f"Sua altura é {altura}m")
