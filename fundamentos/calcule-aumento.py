8c40770227e2353a872756b26c74463821ed5031
anilucia <amilucia07@gmail.com>
2026-03-13 21:29:40 -0300

    Aumento de salário

--- fundamentos/calcule-aumento.py
+++ fundamentos/calcule-aumento.py
@@ -3,3 +3,8 @@ salario = float(input("Digite o salário: "))
 percentual_aumento = float(input("Digite o percentual de aumento: "))
 novo_salario = salario + (salario * percentual_aumento / 100)
 print(f"O novo salário é {novo_salario:.2f}.")
+
+#output do programa será:S
+# Digite o salário: 3500.0
+# Digite o percentual de aumento: 20.0
+# O novo salário é 4200.00.
