5a435d28a4888515534411f201b69a2ec0b3ecf8
anilucia <amilucia07@gmail.com>
2026-03-13 21:07:18 -0300

    Ler o preço

---
+++ fundamentos/ler-imprimir.py
@@ -0,0 +1,5 @@
+#9.Leia o preço de um produto e imprima o preço com 10% de desconto.
+preco = float(input("Digite o preço do produto: "))
+desconto = preco * 0.10
+preco_com_desconto = preco - desconto
+print(f"O preço com 10% de desconto é {preco_com_desconto:.2f}.")
