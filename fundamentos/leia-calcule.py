08196436f221620d43b23067cd7fefdf6d253451
anilucia <amilucia07@gmail.com>
2026-03-13 21:09:49 -0300

    Ler o raio e calcular a area

---
+++ fundamentos/leia-calcule.py
@@ -0,0 +1,5 @@
+#10.Leia o raio de um círculo (float) e calcule a área, utilize π = 3.14.
+raio = float(input("Digite o raio do círculo: "))
+pi = 3.14
+area = pi * (raio ** 2)
+print(f"A área do círculo é {area:.2f}.")
