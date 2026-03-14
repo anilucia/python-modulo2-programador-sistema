46ce293a577fa10c32ad68ea4956b91041be0771
anilucia <amilucia07@gmail.com>
2026-03-13 22:21:58 -0300

    Ano de nascimento

---
+++ fundamentos/ano-nascimento.py
@@ -0,0 +1,4 @@
+#19.Leia o ano de nascimento (int) e imprima a idade estimada. (considere ano atual = 2026).
+ano_nascimento = int(input("Digite o ano de nascimento: "))
+idade = 2026 - ano_nascimento
+print(f"Idade estimada: {idade} anos")
