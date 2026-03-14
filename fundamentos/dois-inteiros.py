676dfb9727afb9e8c3bf1025fa0d267002585a41
anilucia <amilucia07@gmail.com>
2026-03-13 21:54:12 -0300

    Ler inteiros

---
+++ fundamentos/dois-inteiros.py
@@ -0,0 +1,7 @@
+#Leia dois int (a e b) e imprima: a + b = X | a - b = Y | a * b = Z
+a = int(input("Digite o primeiro número inteiro: "))
+b = int(input("Digite o segundo número inteiro: ")) 
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+print(f"{a} + {b} = {soma} | {a} - {b} = {subtracao} | {a} * {b} = {multiplicacao}")
